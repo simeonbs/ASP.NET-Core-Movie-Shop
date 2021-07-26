d6118f990a1ea0bfa8f8dd99d4c8ae44eb013c87
simeonbs <simeonstoimenov@gmail.com>
2021-07-26 12:46:56 +0300

    Made some changes on the index page. *Added carousel listing for the last 3 movies. *Added jumbotron for total movies and total users.

--- MovieShopSystem/MovieShopSystem/Controllers/HomeController.cs
+++ MovieShopSystem/MovieShopSystem/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieShopSystem.Data;
 using MovieShopSystem.Models;
+using MovieShopSystem.Models.Home;
 using MovieShopSystem.Models.Movies;
 using System.Diagnostics;
 using System.Linq;
@@ -11,31 +12,32 @@ namespace MovieShopSystem.Controllers
     {
         private MoviesDbContext data;
 
-        public HomeController(MoviesDbContext data)
-        {
-            this.data = data;
-        }
+        public HomeController(MoviesDbContext data) 
+            => this.data = data;
 
         public IActionResult Index()
         {
+            var totalMovies = this.data.Movies.Count();
+
             var movies = this.data
                 .Movies
                 .OrderByDescending(m => m.Id)
-                .Select(m => new MovieListingViewModel
+                .Select(m => new MovieIndexViewModel
                 {
                     Id = m.Id,
                     Title = m.Title,
-                    YearReleased = m.YearReleased,
-                    Description = m.Description,
-                    Director = m.Director,
+                    ReleasedYear = m.YearReleased,
                     Writer = m.Writer,
                     ImageUrl = m.ImageUrl,
-                    Genre = m.Genre.Name
                 })
                 .Take(3)
                 .ToList();
 
-            return View(movies);
+            return View(new IndexViewModel
+            {
+                TotalMovies = totalMovies,
+                Movies = movies
+            });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
